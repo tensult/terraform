074b876974a8a528dccd7bfcdc82d7053ba7f506
Jawad <jawad@tensult.com>
2018-10-22 19:32:51 +0530

    Add cross account role script

--- aws/cross-account-role/provider.tf
+++
@@ -1,9 +0,0 @@
-# Define AWS as our provider
-provider "aws" {
-  assume_role {
-    role_arn     = "arn:aws:iam::304943532825:role/Payer_to_DemoPOC"
-    
-  }
-  region = "ap-south-1"
-  profile = "master"
-}
