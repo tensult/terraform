be10685e453ecf146d8dc11038d0159f37a3251b
Mohamed Jawad P <33896731+jawad846@users.noreply.github.com>
2018-10-22 17:43:08 +0530

    Update rt.tf

--- aws/vpc/rt.tf
+++ aws/vpc/rt.tf
@@ -26,7 +26,11 @@ resource "aws_route_table_association" "public-rt-2" {
 resource "aws_route_table" "rt-private-ss" {
   vpc_id = "${aws_vpc.shared.id}"
 
-  tags {
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_nat_gateway.nat.id}"
+  }
+    tags {
     Name = "rt-private"
   }
 }
