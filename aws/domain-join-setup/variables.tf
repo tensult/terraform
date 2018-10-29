1b8682554f5e10742aabef121d6a14bc39deff9e
Dilip Kola <dilip@tensult.com>
2018-10-29 18:17:18 +0530

    Add domain join setup for windows

--- aws/domain-join-setup/variables.tf
+++ aws/domain-join-setup/variables.tf
@@ -7,14 +7,18 @@ variable "region" {
   default     = "ap-south-1"
 }
 
+variable "domain_name" {
+  description = "Domain name"
+}
+
+variable "domain_dns_ip" {
+  description = "Domain DNS IP"
+}
+
 variable "domain_username" {
   description = "Domain username"
 }
 
 variable "domain_password" {
   description = "Domain password"
-}
-
-variable "domain_name" {
-  description = "Domain name"
 }
