6b98fa41dc8d4d51a04b708c8e9e23ded86c88a8
Dilip Kola <dilip@tensult.com>
2018-11-13 15:16:53 +0530

    Add ebs snapshot scripts using dlm

---
+++ aws/ebs-snapshots-setup/main.tf
@@ -0,0 +1,56 @@
+provider "aws" {
+  profile = "${var.profile}"
+  region  = "${var.region}"
+}
+
+resource "aws_iam_role" "dlm_lifecycle_role" {
+  name = "dlm-lifecycle-role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "dlm.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_dlm_lifecycle_policy" "snapshot_policy" {
+  description        = "DLM lifecycle policy"
+  execution_role_arn = "${aws_iam_role.dlm_lifecycle_role.arn}"
+  state              = "ENABLED"
+
+  policy_details {
+    resource_types = ["VOLUME"]
+
+    schedule {
+      name = "2 weeks of daily snapshots"
+
+      create_rule {
+        interval      = 24
+        interval_unit = "HOURS"
+        times         = ["23:45"]
+      }
+
+      retain_rule {
+        count = 14
+      }
+
+      tags_to_add {
+        SnapshotCreator = "DLM"
+      }
+    }
+
+    target_tags {
+      Snapshot = "true"
+    }
+  }
+}
