3ae5b7412502363511171fa349314d6c141db991
Dilip Kola <dilip@tensult.com>
2018-11-17 12:56:00 +0530

    Use powershell instead of custom document for AV update

--- aws/windows-antivirus-update-ssm-automation/ssm.tf
+++ aws/windows-antivirus-update-ssm-automation/ssm.tf
@@ -5,27 +5,3 @@ resource "aws_ssm_document" "av_update_automation" {
   content = "${file("windows_av_update_automation_document.json")}"
 }
 
-resource "aws_ssm_document" "av_update_run_command" {
-  name          = "Windows_Antivirus_Update_Run_Command"
-  document_type = "Command"
-
-  content = <<DOC
-  {
-      "schemaVersion":"2.0",
-      "description":"Run a PowerShell script to update Antivirus a Windows instance",
-      "mainSteps":[
-         {
-            "action":"aws:runPowerShellScript",
-            "name":"runPowerShellWithSecureString",
-            "inputs":{
-               "runCommand":[
-                  "& \"C:\Program Files\McAfee\Agent\cmdagent.exe\" -c\n",
-                  "Start-Sleep -s 30"
-               ]
-            }
-         }
-      ]
-   }
-DOC
-}
-
