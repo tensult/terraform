6f7bc867159c32bcc20649939c1646c1f4583615
Dilip Kola <dilip@tensult.com>
2018-11-19 18:55:41 +0530

    Add cloudwatch event rule for launch automation

---
+++ aws/ec2-launch-automation/cloudwatch.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudwatch_event_rule" "ec2_run_instances" {
+  name        = "EC2-Instance-Launch-Automation"
+  description = "Runs automation on EC2 instance launch"
+  event_pattern = <<PATTERN
+{
+  "source": [
+    "aws.ec2"
+  ],
+  "detail-type": [
+    "AWS API Call via CloudTrail"
+  ],
+  "detail": {
+    "eventSource": [
+      "ec2.amazonaws.com"
+    ],
+    "eventName": [
+      "RunInstances"
+    ]
+  }
+}
+PATTERN
+}
+resource "aws_cloudwatch_event_target" "ec2_run_instances" {
+  rule      = "${aws_cloudwatch_event_rule.ec2_run_instances.name}"
+  arn       = "${aws_lambda_function.lambda_function.arn}"
+}
