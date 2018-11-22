920d1f159236c488a1d71e3be2c7e7b4f3624dfb
Anirudh Baskaran <anirudh@tensult.com>
2018-11-23 00:32:10 +0530

    Added Maintenance Window and Task Creation

--- aws/ssm-patch-window/main.tf
+++ aws/ssm-patch-window/main.tf
@@ -8,3 +8,36 @@ resource "aws_ssm_maintenance_window" "window" {
   duration = "${var.duration}"
   cutoff   = "${var.cutoff_time}"
 }
+
+resource "aws_ssm_maintenance_window_target" "target1" {
+  window_id     = "${aws_ssm_maintenance_window.window.id}"
+  resource_type = "INSTANCE"
+
+  targets {
+    key    = "tag:os_type"
+    values = ["RedHat6","CentOS6","AmazonLinux2","Ubuntu16"]
+  }
+}
+
+
+resource "aws_ssm_maintenance_window_task" "task" {
+  window_id        = "${aws_ssm_maintenance_window.window.id}"
+  name             = "Run-Patch-Baseline-Document"
+  description      = "Task to Install Patches to Linux Instances"
+  task_type        = "RUN_COMMAND"
+  task_arn         = "AWS-RunPatchBaseline"
+  priority         = 1
+  service_role_arn = "arn:aws:iam::${var.account_id}:role/aws-service-role/ssm.amazonaws.com/AWSServiceRoleForAmazonSSM"
+  max_concurrency  = "3"
+  max_errors       = "10"
+
+  targets {
+    key    = "WindowTargetIds"
+    values = ["${aws_ssm_maintenance_window_target.target1.id}"]
+  }
+
+  task_parameters {
+    name   = "Operation"
+    values = ["Install"]
+  }
+}
--- aws/ssm-patch-window/variables.tf
+++ aws/ssm-patch-window/variables.tf
@@ -9,13 +9,20 @@ variable "region" {
 
 variable "cutoff_time" {
   description = "Time in hours before end of Patch Maintenance Window when new execution will not be scheduled"
+  default = 1
 }
 
 variable "duration" {
   description = "Patch Maintenance Window Duration in hours"
-  
+  default = 7
 }
 variable "cron" {
   description = "Cron expression for deciding window scheduling"
-  
+  type = "string"
+  #The default configured below is cron for running every 2nd Saturday of the month at 10:00 AM
+  default = "cron(0 0 10 ? * 7#2)"
+ }
+
+ variable "account_id" {
+  description = "AccountID"
 }
