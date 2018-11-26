d3b5d73e36ebd3cc464c2e1cddf1ab150d150cc0
Jawad <jawad@tensult.com>
2018-11-26 17:04:32 +0530

    Updated ec2-launch

--- aws/ec2-launch-automation/ssm.tf
+++ aws/ec2-launch-automation/ssm.tf
@@ -1,3 +1,48 @@
+data "aws_kms_key" "ssm" {
+  key_id = "alias/ssm-key"
+}
+
+resource "aws_ssm_parameter" "domain_username" {
+  name  = "/domain/username"
+  description  = "Domain username"
+  type  = "String"
+  value = "${var.domain_username}"
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "domain_password" {
+  name  = "/domain/password"
+  description  = "Domain password"
+  type  = "SecureString"
+  value = "${var.domain_password}"
+  key_id = "${data.aws_kms_key.ssm.arn}"
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "ipdns" {
+  name  = "/domain/dns_ip"
+  description  = "DNS IP Address"
+  type  = "String"
+  value = "${join(",", var.domain_dns_ips)}"
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "domain_name" {
+  name  = "/domain/name"
+  description  = "Domain name"
+  type  = "String"
+  value = "${var.domain_name}"
+  overwrite = true
+}
+
+resource "aws_ssm_parameter" "domain_ou_path" {
+  name  = "/domain/ou_path"
+  description  = "Domain OU path"
+  type  = "String"
+  value = "${var.domain_ou_path}"
+  overwrite = true
+}
+
 resource "aws_ssm_document" "amazon_linux_automation" {
   name          = "Launch_Automation_for_AmazonLinux"
   document_type = "Automation"
--- aws/ec2-launch-automation/variables.tf
+++ aws/ec2-launch-automation/variables.tf
@@ -6,3 +6,34 @@ variable "region" {
   description = "AWS region"
   default     = "ap-south-1"
 }
+
+variable "vpc_id" {
+  description = "VPC ID"
+}
+
+variable "domain_name" {
+  description = "Domain name"
+}
+
+variable "domain_ou_path" {
+  description = "Domain OU path"
+}
+
+variable "domain_dns_ips" {
+  description = "Domain DNS IPs"
+  type = "list"
+}
+
+variable "amazon_dns" {
+  description = "Amazon Provided DNS"
+  default = ["AmazonProvidedDNS"],
+  type = "list"
+}
+
+variable "domain_username" {
+  description = "Domain username"
+}
+
+variable "domain_password" {
+  description = "Domain password"
+}
