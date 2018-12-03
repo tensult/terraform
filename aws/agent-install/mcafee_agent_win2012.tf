ecb8e58b18df6f6ab49f255e9a73890b65ccceea
Jawad <jawad@tensult.com>
2018-12-03 12:11:12 +0530

    Update Agent Insatll

--- aws/agent-install/mcafee_agent_win2012.tf
+++ aws/agent-install/mcafee_agent_win2012.tf
@@ -18,7 +18,7 @@ resource "aws_ssm_document" "mcafee_agent_windows2012" {
                   "$mcafee = ('C:\\Temp\\McAfee_Endpoint_Security_10_5_4_4035_15_stand_alone_client_install.Zip')\n",
                   "Add-Type -AssemblyName System.IO.Compression.FileSystem\n",
                   "Wait-Event -Timeout 10\n",
-                  "[System.IO.Compression.ZipFile]::ExtractToDirectory($mcafee, 'C:\\Temp\\McAfee\\')\n",
+                  "[System.IO.Compression.ZipFile]::ExtractToDirectory($mcafee, 'C:\\Temp\\McAfee\\')\n"
                ]
             }
          },
@@ -28,12 +28,13 @@ resource "aws_ssm_document" "mcafee_agent_windows2012" {
             "inputs":{
                "runCommand":[
                   "$endpoint = ('C:\\Temp\\McAfee\\Endpoint Security Platform 10.5.4 Build 4214 Package #5 PATCH Repost (AAA-LICENSED-RELEASE-PATCH ).Zip')\n",
+                  "Add-Type -AssemblyName System.IO.Compression.FileSystem\n",
                   "Wait-Event -Timeout 10\n",
                   "[System.IO.Compression.ZipFile]::ExtractToDirectory($endpoint, 'C:\\Temp\\McAfee\\EndpointSecurityPlatform10.5.4\\')\n",
                   "Wait-Event -Timeout 30\n",
                   "& 'C:\\Temp\\McAfee\\EndpointSecurityPlatform10.5.4\\setupCC.exe'\n",
                   "Wait-Event -Timeout 60\n",
-                  "msiexec.exe /i 'C:\\Temp\\McAfee\\EndpointSecurityPlatform10.5.4\\McAfee_Common_x64.msi /l*v 'C:\\Temp\\logs\\McAfee_Common_x64.log' /qn\n",
+                  "msiexec.exe /i 'C:\\Temp\\McAfee\\EndpointSecurityPlatform10.5.4\\McAfee_Common_x64.msi /l*v 'C:\\Temp\\logs\\McAfee_Common_x64.log' /qn\n"
                ]
             }
          },
@@ -43,12 +44,13 @@ resource "aws_ssm_document" "mcafee_agent_windows2012" {
             "inputs":{
                "runCommand":[
                   "$firewall = ('C:\\Temp\\McAfee\\Firewall 10.5.4 Build 4179 Package #1 PATCH Repost (AAA-LICENSED-RELEASE-PATCH ).Zip')\n",
+                  "Add-Type -AssemblyName System.IO.Compression.FileSystem\n",
                   "Wait-Event -Timeout 10\n",
                   "[System.IO.Compression.ZipFile]::ExtractToDirectory($firewall, 'C:\\Temp\\McAfee\\Firewall10.5.4\\')\n",
                   "Wait-Event -Timeout 30\n",
                   "& 'C:\\Temp\\McAfee\\Firewall10.5.4\\setupFW.exe'\n",
                   "Wait-Event -Timeout 60\n",
-                  "msiexec.exe /i 'C:\\Temp\\McAfee\\Firewall10.5.4\\McAfee_Firewall_x64.msi' /l*v 'C:\\Temp\\logs\\McAfee_Firewall.log' /qn\n",
+                  "msiexec.exe /i 'C:\\Temp\\McAfee\\Firewall10.5.4\\McAfee_Firewall_x64.msi' /l*v 'C:\\Temp\\logs\\McAfee_Firewall.log' /qn\n"
                ]
             }
          },
@@ -58,12 +60,13 @@ resource "aws_ssm_document" "mcafee_agent_windows2012" {
             "inputs":{
                "runCommand":[
                   "$threat = ('C:\\Temp\\McAfee\\Threat Prevention 10.5.4 Build 4240 Package #4 PATCH Repost (AAA-LICENSED-RELEASE-PATCH ).Zip')\n",
+                  "Add-Type -AssemblyName System.IO.Compression.FileSystem\n",
                   "Wait-Event -Timeout 10\n",
                   "[System.IO.Compression.ZipFile]::ExtractToDirectory($threat, 'C:\\Temp\\McAfee\\ThreatPrevention10.5.4\\')\n",
                   "Wait-Event -Timeout 30\n",
                   "& 'C:\\Temp\\McAfee\\ThreatPrevention10.5.4\\setupTP.exe'\n",
                   "Wait-Event -Timeout 60\n",
-                  "msiexec.exe /i 'C:\\Temp\\McAfee\\ThreatPrevention10.5.4\\McAfee_Threat_Prevention_x64.msi' /l*v 'C:\\Temp\\logs\\McAfee_Threat_Prevention.log' /qn\n",
+                  "msiexec.exe /i 'C:\\Temp\\McAfee\\ThreatPrevention10.5.4\\McAfee_Threat_Prevention_x64.msi' /l*v 'C:\\Temp\\logs\\McAfee_Threat_Prevention.log' /qn\n"
                ]
             }
          },
@@ -73,6 +76,7 @@ resource "aws_ssm_document" "mcafee_agent_windows2012" {
             "inputs":{
                "runCommand":[
                   "$webcontrol = ('C:\\Temp\\McAfee\\Web Control 10.5.4 Build 4177 Package #1 PATCH Repost (AAA-LICENSED-RELEASE-PATCH ).Zip')\n",
+                  "Add-Type -AssemblyName System.IO.Compression.FileSystem\n",
                   "Wait-Event -Timeout 10\n",
                   "[System.IO.Compression.ZipFile]::ExtractToDirectory($webcontrol, 'C:\\Temp\\McAfee\\WebControl10.5.4\\')\n",
                   "Wait-Event -Timeout 30\n",
