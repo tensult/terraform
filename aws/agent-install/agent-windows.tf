06707c66dbd90ecfd059f3a0e84b18f634e75df3
Dilip Kola <dilip@tensult.com>
2018-12-04 11:49:07 +0530

    Refactor windows CLI install document

--- aws/agent-install/agent-windows.tf
+++ aws/agent-install/agent-windows.tf
@@ -8,12 +8,24 @@ resource "aws_ssm_document" "windows_awscli" {
      "description": "Run a script to securely install Agent in Windows 2012 instance",
      "mainSteps": [
         {
+            "action":"aws:runPowerShellScript",
+            "name": "downloadAWSCLI",
+            "inputs":{
+               "runCommand":[
+                  "mkdir 'C:\\Temp'\n",
+                  "Invoke-WebRequest -Uri \"https://s3.amazonaws.com/aws-cli/AWSCLI64PY3.msi\" -OutFile 'C:\\Temp\\AWS_CLI.msi'\n",
+                  "dir 'C:\\Temp\\'\n",
+                  "echo 'AWS CLI is downloaded.'"
+               ]
+            }
+        },
+        {
            "action": "aws:applications",
            "name": "installApplication",
            "inputs": {
               "parameters": "/quiet",
               "action": "Install",
-              "source": "https://s3.amazonaws.com/aws-cli/AWSCLI64PY3.msi"
+              "source": "C:\\Temp\\AWS_CLI.msi"
             }
          },
          {
@@ -24,6 +36,7 @@ resource "aws_ssm_document" "windows_awscli" {
                   "while (!(Test-Path 'C:\\Program Files\\Amazon\\AWSCLI\\bin\\aws.cmd')){ Wait-Event -Timeout 10}\n",
                   "& 'C:\\Program Files\\Amazon\\AWSCLI\\bin\\aws.cmd'\n",
                   "echo 'AWS CLI is installed.'\n",
+                  "Remove-Item -Path 'C:\\Temp\\AWS_CLI.msi'\n",
                   "Restart-Computer -Force"
                ]
             }
