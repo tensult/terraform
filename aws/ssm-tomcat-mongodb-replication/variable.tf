7a94d849f2143acdc1b408b9b0557c6309dd2e89
Jawad <jawad@tensult.com>
2019-01-15 13:15:49 +0530

    Install Tomcat & MongoDB using SSM and Replication

---
+++ aws/ssm-tomcat-mongodb-replication/variable.tf
@@ -0,0 +1,13 @@
+variable "profile" {
+  description = "AWS Profile"
+  default = "default"
+}
+
+variable "region" {
+  description = "AWS Region"
+  default = "us-east-1"
+}
+
+variable "customer" {
+  description = "Name of the customer"
+}
