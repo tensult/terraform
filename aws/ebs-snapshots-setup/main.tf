09d9842aa29829a6bbed94849d2f851c855ff85e
Sandeep Harikumar <sandeep@Sandeeps-MacBook-Air-2.local>
2019-01-16 18:24:11 +0530

    Updated ebs-snapshot-setup

--- aws/ebs-snapshots-setup/main.tf
+++ aws/ebs-snapshots-setup/main.tf
@@ -7,27 +7,18 @@ resource "aws_iam_role" "dlm_lifecycle_role" {
   name = "dlm-lifecycle-role"
 
   assume_role_policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:CreateSnapshot",
-                "ec2:DeleteSnapshot",
-                "ec2:DescribeVolumes",
-                "ec2:DescribeSnapshots"
-            ],
-            "Resource": "*"
-        },
-        {
-            "Effect": "Allow",
-            "Action": [
-                "ec2:CreateTags"
-            ],
-            "Resource": "arn:aws:ec2:*::snapshot/*"
-        }
-    ]
+  {
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+      "Service": "dlm.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
 }
 EOF
 }
