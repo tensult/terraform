b692894f5de6ea49b3cb8b139a828a799be34ec2
Dilip Kola <dilip@tensult.com>
2019-01-28 14:46:14 +0530

    Update iam cross account role

--- aws/iam-cross-account-role/main.tf
+++ aws/iam-cross-account-role/main.tf
@@ -18,6 +18,33 @@ resource "aws_iam_role" "cross_account" {
 EOF
 }
 
+resource "aws_iam_policy" "iam_permission" {
+  name        = "IAM_Role_Permissions_For_Tensult"
+  description = "Basic role permission for tensult"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "iam:PassRole",
+                "iam:CreateRole",
+                "iam:AddRoleToInstanceProfile",
+                "iam:AttachRolePolicy",
+                "iam:CreatePolicy",
+                "iam:CreateInstanceProfile",
+                "sts:GetCallerIdentity"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
 #Attach policy
 resource "aws_iam_role_policy_attachment" "permission" {
   role       = "${aws_iam_role.cross_account.name}"
@@ -27,4 +54,9 @@ resource "aws_iam_role_policy_attachment" "permission" {
 resource "aws_iam_role_policy_attachment" "read_permission" {
   role       = "${aws_iam_role.cross_account.name}"
   policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "iam_permission" {
+  role       = "${aws_iam_role.cross_account.name}"
+  policy_arn = "${aws_iam_policy.iam_permission.arn}"
 }
